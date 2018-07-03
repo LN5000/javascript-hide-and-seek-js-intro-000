c19a64e8f14fa46e0be48df1482e461631c96483
Lee <lee.o.noble@gmail.com>
2018-07-03 07:51:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,34 +42,13 @@ function deepestChild() {
 
 // ----
 
-const main = document.getElementById('app')
-main.addEventListener('click', function(event) {
-  alert('I was clicked!')
-})
+document.addEventListener("keydown", keyDownTextField, false);
 
-// Keys for A, B, and C keys.
-const alphabet = ['a', 'b', 'c'];
-
-// Keep track of index outside of the event handler.
-let index = 0;
-
-// This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
-  const key = e.key;
-
-  if (key === alphabet[index]) {
-    index++;
-
-    if (index === alphabet.length) {
-      alert("Hurray!");
-
-      index = 0;
-    }
+function keyDownTextField(e) {
+var keyCode = e.keyCode;
+  if(keyCode==13) {
+  alert("You hit the enter key.");
   } else {
-    index = 0;
+  alert("Oh no you didn't.");
   }
 }
-
-$(document).addEventListener('keydown', onKeyDownHandler {
-  alert('I was clicked!')
-})
